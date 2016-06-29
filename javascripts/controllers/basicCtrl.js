4c86066e3990bb89697dafb3712ac2626cacdfb0
unknown <15041622@CNHQ-15041622N.sn.suning.ad>
2016-06-29 19:21:36 +0800

    纪念

--- javascripts/controllers/basicCtrl.js
+++ javascripts/controllers/basicCtrl.js
@@ -1,5 +1,7 @@
 angular.module('kaka.cblog').controller('BasicCtrl', ['$scope', 
   function($scope){
-
+    var start = new Date('2016/5/21 00:00:00');
+    var now = Date.now();
+    $scope.distance = Math.ceil((now - start)/3600000/24);
   }
 ]);
